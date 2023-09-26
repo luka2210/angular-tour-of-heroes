868d1a8e3cc01a067b5f9d1cffe79e71085a8bc2
luka2210 <lukamomcilovic221099@gmail.com>
2023-09-26 10:32:43 +0200

    added message component; created services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../services/hero.service';
+import { MessageService } from '../services/message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,10 +9,28 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  heroes: Hero[] = HEROES
+  heroes?: Hero[]
   selectedHero?: Hero
+
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
+
+  ngOnInit(): void {
+    this.getHeroes()
+  }
+
+  getHeroes(): void {
+    this.heroService.getAllHeroes().subscribe({
+      next: content => {
+        this.heroes = content
+      },
+      error: err => {
+        console.log(err)
+      }
+    })
+  }
 
   onSelect(hero: Hero):void {
     this.selectedHero = hero
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
